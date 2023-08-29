7ae59a266edc57a745a64660a7d068ce347bd15b
Alex <alex.rybicki@gmail.com>
2023-08-29 11:16:38 -0700

    Update flip.js

--- assets/JS/flip.js
+++ assets/JS/flip.js
@@ -2,5 +2,5 @@ var card = document.querySelector('.card');
 if (card !== undefined && card !== null) {
   card.addEventListener( 'click', function() {
   card.classList.toggle('is-flipped');
- }
-});
+ })
+};
